d5cdc2919e9976fa2b171a68e75e755e347bbf52
Philipp A <flying-sheep@web.de>
2017-11-12 21:30:02 +0100

    Added basics for tabulation

--- R/table-react.r
+++ R/table-react.r
@@ -10,16 +10,60 @@ table_react <- function(id) {
 #' @importFrom purrr compact
 #' @export
 table_react_select <- function(session, id, selected = NULL) {
-	session$sendInputMessage(id, compact(list(value = selected)))
+	session$sendInputMessage(id, compact(list(
+		value = selected_to_js(selected))))
 }
 
-table_react_update <- function(session, id, columns = NULL, selected = NULL) {
+PAGE_LENGTH = 10L
+
+#' @importFrom shiny validate need
+table_react_update <- function(session, id, data = NULL, selected = NULL, page = NULL) {
+	if (!can_convert_to_df(data)) {
+		session$sendCustomMessage(type = 'update-table-react', list(
+			id = id, error = paste('Ragged array:\n', paste0(capture.output(str(data)), collapse = '\n'))))
+		return()
+	}
+	
+	page_idx <- if (is.null(page)) NULL else page - 1L
+	
+	page_start <- if (is.null(page_idx)) 0L else page_idx * PAGE_LENGTH  # before!
+	rows <- seq.int(
+		from = page_start + 1L,
+		to = min(page_start + PAGE_LENGTH, nrow(data)))
+	
+	columns <-
+		if (!is.null(nrow(data))) as.data.frame(data[rows, , drop = FALSE])
+		else as.data.frame(data)[rows, , drop = FALSE]
+	
 	session$sendCustomMessage(type = 'update-table-react', compact(list(
-		id = id, columns = columns, selected = selected)))
+		id = id,
+		columns = columns,
+		selected = selected_to_js(selected),
+		page = page_idx,
+		n_pages = ceiling(nrow(data) / PAGE_LENGTH))))
+}
+
+can_convert_to_df <- function(thing) {
+	if (!is.list(thing)) return(TRUE)  # TODO
+	first_length <- length(thing[[1L]])
+	all(sapply(thing, function(t) length(t) == first_length))
+}
+
+selected_to_js <- function(selected) {
+	if (is.null(selected)) return(NULL)
+	if (!is.integer(selected)) stop('Can only select row indices by now')
+	selected - 1L
+}
+
+page_to_js <- function(page) {
+	if (is.null(page)) return(NULL)
+	page - 1L
 }
 
 register_handler <- function() {
-	registerInputHandler('shiny.table.react', function(data, ...) as.integer(data), force = TRUE)
+	registerInputHandler('shiny.table.react', function(data, ...) {
+		as.integer(data) + 1L
+	}, force = TRUE)
 }
 
 deps <- function(dev = TRUE) list(
--- inst/www/table-bindings.jsx
+++ inst/www/table-bindings.jsx
@@ -5,6 +5,8 @@ const dev = true
 const get_empty_state = () => ({
 	selected: [],
 	columns:  {},
+	page:     0,
+	n_pages:  1,
 	callback: null,
 	error:    null,
 })
@@ -19,37 +21,16 @@ class Table extends React.Component {
 		this.setState({error: `${error.toString()}\n${info.componentStack}`})
 	}
 	
-	shouldComponentUpdate(nextProps, nextState) {
-		const {columns, error} = nextState
-
-		if (dev && !error) {
-			const lengths = Object.entries(columns)
-				.map(([header, column]) => ({header, l: column.length}))
-				.reduce((lengths, {header, l}) => {
-					lengths[l] = l in lengths ? [...lengths[l], header] : [header]
-					return lengths
-				}, {})
-			if (Object.keys(lengths).length > 1) {
-				const err = new Error(`Ragged data passed! Encountered column lengths:\n${
-					Object.entries(lengths).map(([l, headers]) => `${l}: ${headers.join(', ')}`).join('\n')
-				}`)
-				this.setState({error: `${err.toString()}\n${err.stack}`})
-			}
-		}
-
-		return true
-	}
-	
 	render() {
-		const {columns, error, callback, selected} = this.state
+		const {columns, page, n_pages, error, callback, selected} = this.state
 
 		if (error) return <pre className="error">{error}</pre>
 
 		const cols = Object.values(columns)
 		const ncol = cols.length
 		const nrow = (cols[0] || []).length  // raggedness checked in shouldComponentUpdate
-		return (
-			<table className="react-table">
+		return [
+			<table className="react-table" key="table">
 				<thead>
 					<tr>{Object.keys(columns).map(header =>
 						<th key={header}>{header}</th>
@@ -57,14 +38,17 @@ class Table extends React.Component {
 				</thead>
 				<tbody>{Array(nrow).fill(null).map((_, r) =>
 					<tr key={r}
-						className={selected.includes(r) ? 'selected' : null}
-						onClick={() => this.select_toggle(r)}
+						className={selected.includes(r - page) ? 'selected' : null}
+						onClick={() => this.select_toggle(r - page)}
 					>{Array(ncol).fill(null).map((_, c) =>
 						<td key={c}>{cols[c][r]}</td>
 					)}</tr>
 				)}</tbody>
-			</table>
-		)
+			</table>,
+			<div key="pages">{Array(n_pages).fill(null).map((_, p) =>
+				<button key={p} disabled={p === page}>{p + 1}</button>
+			)}</div>,
+		]
 	}
 	
 	select_toggle(row) {
